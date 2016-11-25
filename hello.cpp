0116e8886d30e10d9fcb4029a01ca196846e2124
xuhuichi <550329585@qq.com>
2016-11-25 14:53:41 +0800

    添加新文件

---
+++ hello.cpp
@@ -0,0 +1,15 @@
+/*************************************************************************
+	> File Name: hello.cpp
+	> Author: xuhuichi
+	> Desc:  
+	> Created Time: 2016年11月25日 星期五 14时50分45秒
+ ************************************************************************/
+
+#include <iostream>
+#include <stdint.h>
+
+int32_t main()
+{
+	std::cout << "hello,worl" << std::endl;
+	return 0;
+}
