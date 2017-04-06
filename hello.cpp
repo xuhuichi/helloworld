dff754aebc1f4859d579d22407320e5ace04129d
xuhuichi <550329585@qq.com>
2017-04-06 16:20:00 +0800

    aa

--- hello.cpp
+++ hello.cpp
@@ -13,9 +13,6 @@ int32_t main()
 	std::cout << "hello,worl" << std::endl;
 	std::cout << "hello,wor2" << std::endl;
 	std::cout << "hello,wor3" << std::endl;
-<<<<<<< HEAD
-=======
 	std::cout << "hello,wor4" << std::endl;
->>>>>>> trunk
 	return 0;
 }
